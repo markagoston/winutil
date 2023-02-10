f2f00c2121cb361a86f88d21d59e45da62488818
Mark Agoston <mark.agoston77@gmail.com>
2023-02-10 09:09:24 +0100

    bat file added

---
+++ oneclick.bat
@@ -0,0 +1,3 @@
+powershell curl.exe https://raw.githubusercontent.com/markagoston/winutil/main/ctt-oneclick.ps1 -o ctt.ps1
+powershell -ExecutionPolicy Bypass -NoProfile -File ctt.ps1
+del ./ctt.ps1
